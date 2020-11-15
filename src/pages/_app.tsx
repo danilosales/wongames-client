77156dd7a37267b76abf8d63b767b87479e819f4
Danilo Sales <danilosales.info@gmail.com>
2020-11-15 18:17:21 -0300

    configure theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import Head from 'next/head'
+import theme from 'styles/theme'
 
 import GlobalStyles from 'styles/global'
+import { ThemeProvider } from 'styled-components'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
@@ -18,7 +20,7 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
