7df861bbe435e4918d7b921a77fab3d8c73166df
Danilo Sales <danilosales.info@gmail.com>
2020-11-29 16:32:14 -0300

    feat: adjust button to use as a link

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -15,16 +15,25 @@ export default {
   }
 } as Meta
 
-export const Default: Story = (argTypes) => <Button {...argTypes} />
+export const Default: Story = (args) => <Button {...args} />
 
 Default.args = {
   children: 'Buy now'
 }
 
-export const withIcon: Story = (argTypes) => <Button {...argTypes} />
+export const withIcon: Story = (args) => <Button {...args} />
 
 withIcon.args = {
   size: 'small',
   children: 'Buy now',
   icon: <AddShoppingCart />
+}
+
+export const asLink: Story = (args) => <Button {...args} />
+
+asLink.args = {
+  size: 'large',
+  children: 'Buy now',
+  as: 'a',
+  href: '/link'
 }
