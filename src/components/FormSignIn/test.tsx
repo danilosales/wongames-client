fb96c75e4d7452cb664c538d864c68e335ef0e2f
Danilo Alves da Silva Sales <danilo.alves@pixeon.com>
2020-12-30 12:02:49 -0300

    feat: create signin and signup page and some adjusts

---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,34 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import FormSignIn from '.'
+
+describe('<FormSignIn />', () => {
+  it('should render the form', () => {
+    const { container } = renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByPlaceholderText(/Email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/Password/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sign in now/i })
+    ).toBeInTheDocument()
+
+    expect(container.parentElement).toMatchSnapshot()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /forgot your password/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the text and link to sign up', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByText(/Don´t have an account\?/i)).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+  })
+})
